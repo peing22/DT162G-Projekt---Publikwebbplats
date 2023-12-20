6e9144ea9064b8dc219b4b4d613696c4541f3186
Petra Ingemarsson <petra.ingemarsson@gmail.com>
2023-12-20 08:46:33 +0100

    css

--- src/components/background.component.js
+++ src/components/background.component.js
@@ -8,10 +8,10 @@ export default function Background() {
     return (
         <article className="background">
             <h1>Bakgrund</h1>
-            <p>Vi människor är skapade för rörelse. Våra kroppar har sett ut ungefär som de gör idag i cirka 18 000-25 000 års tid. De senaste åren har vårt levnads&shy;sätt dock för&shy;ändrats drastiskt till det sämre, då vi har blivit allt mer stilla&shy;sittande. Vår brist på rörelse påverkar oss inte bara för stunden utan också våra liv på längre sikt.</p>
+            <p>Vi människor är skapade för rörelse. Våra kroppar har sett ut ungefär som de gör idag i cirka 18 000-25 000 års tid. De senaste åren har vårt levnads&shy;sätt dock för&shy;ändrats drastiskt till det sämre. Vi har blivit allt mer stilla&shy;sittande. Vår brist på rörelse påverkar oss inte bara för stunden utan också våra liv på längre sikt.</p>
             <p>Musklerna, skelettet och lederna stärks när de belastas, och kroppen anpassar sig efter de krav den utsätts för. Just muskula&shy;turen tänker vi i första hand på som ett redskap för att röra våra ben och armar, men muskula&shy;turen gör så mycket mer. Musklerna är en källa till pro&shy;duktion av hormoner och andra ämnen som påverkar oss.</p>
             <p>När vi använder våra muskler och tränar dem blir vi inte bara starkare. Vi ökar också vår livs&shy;kvalitet och livs&shy;längd samt före&shy;bygger och minskar risken för sjukdomar såsom diabetes, Alzheimers sjukdom och hjärt- och kärl&shy;sjukdomar.</p>
-            <p>Träningen av våra muskler behöver ske struktu&shy;rerat, på rätt nivå och med rätt svårig&shy;hets&shy;grad för att ge önskad effekt. Denna webbplats innehåller olika <Link to="/ovningar">övningar</Link> som du kan utföra i hemmet eller på gymmet för att uppnå ditt mål.</p>
+            <p>Träningen av våra muskler behöver ske struktu&shy;rerat, på rätt nivå och med rätt svårig&shy;hets&shy;grad för att generera beskrivna fördelar. Denna webbplats innehåller olika <Link to="/ovningar">övningar</Link> som du kan utföra regelbundet, i hemmet eller på gym, för att uppnå önskad effekt.</p>
         </article>
     );
 }
--- src/components/contact.component.js
+++ src/components/contact.component.js
@@ -7,8 +7,8 @@ export default function Contact() {
     return (
         <article className="contact">
             <h1>Kontakt</h1>
-            <p>"Träna för att leva" är ett koncept som jag, Per Ingemarsson, har arbetat fram för att främja god hälsa och livs&shy;kvalitet hos människor. Jag arbetar som fysio&shy;terapeut och har ett stort hjärta för mitt jobb och för de människor jag möter!</p>
-            <p>Min breda kunskap och gedigna erfarenhet omfattar olika aspekter av fysisk aktivitet, från rehabili&shy;tering efter skador till före&shy;byggande övningar för att främja långsiktig hälsa.</p>
+            <p>"Träna för att leva" är ett koncept som jag, Per Ingemarsson, har arbetat fram för att främja god hälsa och livs&shy;kvalitet hos människor. Jag arbetar som fysio&shy;terapeut och har ett stort hjärta för mitt jobb och för att hjälpa de människor jag möter!</p>
+            <p>Min breda kunskap och gedigna erfarenhet omfattar olika aspekter av fysisk aktivitet, från rehabili&shy;tering efter skador till före&shy;byggande övningar för att främja lång&shy;siktig hälsa.</p>
             <p>Du är välkommen att kontakta mig via telefon eller e-post om du har frågor eller är intresserad av att boka en inspirerande före&shy;läsning baserad på mitt koncept.</p>
             <div className="contact-info">
                 <div className="contact-wrap">
